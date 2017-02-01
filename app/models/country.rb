403715a0607d066d61588825ee4b5cfdd5d2db1b
pavel-mihailovskiy <pavel@active-bridge.com>
2017-02-01 14:41:48 +0200

    Add lines between different types of associations

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,6 +1,8 @@
 class Country < ActiveRecord::Base
   belongs_to :panel_provider
+
   has_and_belongs_to_many :target_groups
+
   has_many :location_groups
   has_many :locations, through: :location_groups
 
